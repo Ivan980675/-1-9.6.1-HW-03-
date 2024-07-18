d9930db529392f635e769bb06c9cea80073c53e2
Ivan980675 <141941222+Ivan980675@users.noreply.github.com>
2024-07-18 17:51:55 +0500

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,44 @@
+﻿// Создаем свой тип исключения
+public class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем массив из пяти различных видов исключений
+        Exception[] exceptions = {
+new DivideByZeroException("Деление на ноль"),
+new IndexOutOfRangeException("Индекс вне диапазона"),
+new NullReferenceException("Ссылка на null"),
+new CustomException("Пользовательское исключение"),
+new ArgumentException("Недопустимый аргумент")
+};
+
+        // Реализуем конструкцию TryCatchFinally
+        foreach (var exception in exceptions)
+        {
+            try
+            {
+                ThrowException(exception);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+        }
+    }
+
+    static void ThrowException(Exception exception)
+    {
+        throw exception;
+    }
+}
